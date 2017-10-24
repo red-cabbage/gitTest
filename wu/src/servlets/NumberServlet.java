fd7b4793e4220fb4307630dbb717b809fda46b7f
red-cabbage <1442629049@qq.com>
2017-10-24 22:29:26 +0800

    第一次提交

---
+++ wu/src/servlets/NumberServlet.java
@@ -0,0 +1,82 @@
+package  servlets;
+
+import java.io.IOException
+;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+public class NumberServlet extends HttpServlet {
+
+	/**
+	 * Constructor of the object.
+	 */
+	public NumberServlet() {
+		super();
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		this.doPost(request, response);
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		//response.setContentType("text/html");
+		//PrintWriter out = response.getWriter();
+		HttpSession session=request.getSession();
+		/* if(session!=null){
+		     String myRnd = RndImage.random();//��������ַ�
+		     session.setAttribute("rnd",myRnd); //������ַ�д��Ự״̬
+		     response.setContentType("image/jpeg");//�������Ϊͼ��
+		    RndImage.imageOut(myRnd,response.getOutputStream(),150,50);
+		}*/
+
+		
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
